fb2852919f2a03f1235e984f8969abe9732d7fdd
Mateo Klocke <1999.mpk@gmail.com>
2018-04-27 16:32:28 -0300

    add input value functionality

--- public/index.js
+++ public/index.js
@@ -1,18 +1,25 @@
-// 42.3601 (lat)
-		// 	-71.0589 (long)
-const proxy = 'https://cors-anywhere.herokuapp.com/';
-const API = 'api.openweathermap.org/data/2.5/weather?q=Buenos Aires&appid=7c02b1090f42b77e30cac4cd89ffea05&units=metric';
 
-axios.get('https://' + API)
-	.then(res => {
-		console.log(res.data)
-		let data = res.data,
-			temp = Math.ceil(data.main.temp);
+const city = document.getElementById('city-name');
+const icon = document.getElementById('weather-icon');
 
-		document.getElementById('country-output').textContent = 
-			`
-				Hace ${temp}° en ${data.name} 
-			`;
-	})
-	.catch(err => console.log(err));
+city.addEventListener('keyup', e => {
+	if (e.key === 'Enter') getTemperature(e.target.value)
+	
+});
 
+
+function getTemperature(cityName) {
+	const API = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7c02b1090f42b77e30cac4cd89ffea05&units=metric`;
+
+	axios.get('https://' + API)
+		.then(res => {
+			console.log(res.data)
+			let data = res.data,
+				temp = Math.ceil(data.main.temp);
+
+			document.getElementById('country-output').textContent = temp + '°C';
+
+			icon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+		})
+		.catch(err => console.log(err));
+}
