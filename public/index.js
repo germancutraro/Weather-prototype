a8092ccd6461ef458be4abe4efedb8f57096ce8a
Mateo Klocke <1999.mpk@gmail.com>
2018-04-27 16:39:17 -0300

    we format the code in a clean way

--- public/index.js
+++ public/index.js
@@ -1,25 +1,23 @@
+const city = document.getElementById("city-name");
+const icon = document.getElementById("weather-icon");
+const tempOutput = document.getElementById("country-output");
 
-const city = document.getElementById('city-name');
-const icon = document.getElementById('weather-icon');
-
-city.addEventListener('keyup', e => {
-	if (e.key === 'Enter') getTemperature(e.target.value)
-	
+city.addEventListener("keyup", e => {
+  if (e.key === "Enter") getTemperature(e.target.value);
 });
 
-
 function getTemperature(cityName) {
-	const API = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7c02b1090f42b77e30cac4cd89ffea05&units=metric`;
+  const API = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7c02b1090f42b77e30cac4cd89ffea05&units=metric`;
 
-	axios.get('https://' + API)
-		.then(res => {
-			console.log(res.data)
-			let data = res.data,
-				temp = Math.ceil(data.main.temp);
+  axios.get("https://" + API)
+    .then(res => {
+      console.log(res.data);
+      let data = res.data,
+        temp = Math.ceil(data.main.temp);
 
-			document.getElementById('country-output').textContent = temp + '°C';
+      tempOutput.textContent = temp + "°C";
 
-			icon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-		})
-		.catch(err => console.log(err));
+      icon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    })
+    .catch(err => console.log(err));
 }
