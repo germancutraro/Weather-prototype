b44a1e56a728f9c58886c24cbab0e44533a9c7b1
Mateo Klocke <1999.mpk@gmail.com>
2018-04-27 15:48:17 -0300

    we add express to dependencies

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
+//const cors = require('cors');
 const path = require('path');
-app.use(cors());
+//app.use(cors());
 app.use(express.static(path.join(__dirname, '/public')));
 
 
