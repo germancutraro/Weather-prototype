2a74da06c5dc4f6e5c48a68cd9dc0b53398a9d29
Mateo Klocke <1999.mpk@gmail.com>
2018-04-27 16:04:42 -0300

    Change to open weather map api

--- public/index.js
+++ public/index.js
@@ -1,20 +1,17 @@
 // 42.3601 (lat)
 		// 	-71.0589 (long)
 const proxy = 'https://cors-anywhere.herokuapp.com/';
-const API = 'https://api.darksky.net/forecast/49ff293b41c80f38af83a5248333d4d5/37.8267,-122.4233';
+const API = 'api.openweathermap.org/data/2.5/weather?q=Buenos Aires&appid=7c02b1090f42b77e30cac4cd89ffea05&units=metric';
 
-const farenheitToCelsius = temp => Math.floor((temp - 32) / 1.8);
-
-
-axios.get(proxy + API)
+axios.get('https://' + API)
 	.then(res => {
 		console.log(res.data)
 		let data = res.data,
-			temp = farenheitToCelsius(data.currently.temperature);
+			temp = Math.ceil(data.main.temp);
 
 		document.getElementById('country-output').textContent = 
 			`
-				Hace en ${data.timezone.substring(8, 20)} ${temp}°
+				Hace ${temp}° en ${data.name} 
 			`;
 	})
 	.catch(err => console.log(err));
