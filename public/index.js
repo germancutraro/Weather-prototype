7a2c5bfef9d1643eeb4fb5645145b5e1866ccec9
Mateo Klocke <1999.mpk@gmail.com>
2018-04-27 17:13:16 -0300

    we create a apí key var

--- public/index.js
+++ public/index.js
@@ -1,13 +1,14 @@
 const city = document.getElementById("city-name");
 const icon = document.getElementById("weather-icon");
 const tempOutput = document.getElementById("country-output");
+const API_KEY = '7c02b1090f42b77e30cac4cd89ffea05';
 
 city.addEventListener("keyup", e => {
   if (e.key === "Enter") getTemperature(e.target.value);
 });
 
 function getTemperature(cityName) {
-  const API = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7c02b1090f42b77e30cac4cd89ffea05&units=metric`;
+  const API = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
 
   axios.get("https://" + API)
     .then(res => {
@@ -19,5 +20,5 @@ function getTemperature(cityName) {
 
       icon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     })
-    .catch(err => console.log(err));
+    .catch(err => tempOutput.textContent = "City not found!" );
 }
